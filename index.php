0074121896a51ecccb07155544d3a024c6e3c9e2
zipse <nzipse@slugmail.ucsc.edu>
2011-06-16 14:59:38 -0700

    home page index file and images

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+	
+	<head>
+	
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title> Nangate </title>
+		<link href="style.css" rel="stylesheet" type="text/css" />
+	
+	</head>
+		
+	<body class="w1200">
+		<div class="header">
+			<div id="topnav">
+				<div id="login">
+					<a href="#"> custumer login </a> | <a href="#"> register </a>
+				</div>
+			</div>
+			
+			<div id="topmenu">
+			
+				<div id="logo">
+					<a href="#" title="Back to Homepage"><span></span></a>
+				</div>
+				
+				
+				<div class="mainMenu">
+					
+						  <ul>
+							<li><a href="index.html" title="">Corporate</a>
+							
+									<ul id="subnav-top">               
+										<li><a href="#">Management</a></li>
+										<li><a href="#">Board of Directors</a></li>
+										<li><a href="#">Technical </a></li>
+										<li><a href="#">Partners</a></li>
+										<li><a href="#">Investors</a></li>
+										<li><a href="#">Careers</a></li>
+										<li><a href="#">Events</a></li>
+										<li><a href="#">Press</a></li>
+										<li><a href="#">Legal</a></li>
+									</ul>  
+							</li>
+							<li><a href="#" title="">Products</a></li>
+							<li><a href="#" title="">Support</a></li>
+							<li><a href="#" title="">Open Celll Library</a></li>
+							<li><a href="#" title="">Contact Us</a></li>							
+							<li id="search"> Search <span>GO</span> </li>
+		
+						 </ul>	
+						 
+				</div><!-- /mainMenu -->
+				  
+	
+
+			</div>
+		</div><!-- end header -->
+			
+		<div id="main"><!-- outline container -->
+		
+			<div id="main-top">
+			
+				<div class="content">
+					
+					<div id="content-top">
+					
+					</div>
+					
+					<div id="content-bottom">
+					
+						<div id="left-col">
+							<div id="pic">
+							
+							</div>
+							
+							<div id="info">
+								<p>
+									<h1>Nangate Design Optimizer</h1>
+									You can choose to features a tool in this space.
+									Perhaps Gary's team could load different feature randomly each time the page loads.
+									But convenince offered by predefined digital CMOS standards cells 
+									has come at the prace of overall chip-level efficiency. Standard cell-based ICs are rarely
+									optimized for their intended application, so ICs are not as small, fast
+									or power-effiecient as they could be. <br/><br/>
+									
+									Nangate cell library creation and optimization tools provide the answer.
+								</p>
+								<br/>
+								<br/>
+								<a href="#"> <span></span> Learn more  </a>
+							</div>
+			
+						
+						
+						</div>
+			
+						
+						<div id="right-col">
+						
+							<div id="recent">
+								<h1> <span></span> Recent Headlines </h1>
+								
+								<p>
+									<a href="#"> NDO and Library Creator used by TSMC to improve chip implementation performace</a>
+									<div>
+										March 8, 2011
+									</div>
+								</p>	
+								<p>
+									<a href="#"> NDO and Library Creator used by TSMC to improve chip implementation performace</a>
+									<div>
+										March 8, 2011
+									</div>
+								</p>	
+								<p>
+									<a href="#"> NDO and Library Creator used by TSMC to improve chip implementation performace</a>
+									<div>
+										March 8, 2011
+									</div>
+								</p>
+								
+								<a href="#"> <span></span> More Info  </a>
+							</div>
+							
+						</div><!-- end right col -->
+						
+					</div><!-- end bottom content -->
+				
+				</div><!-- end content -->
+			
+			</div><!-- end main top container -->
+		
+		
+			<div id="main-bottom">	 </div> <!-- bottom part of background image, makes it epandable -->
+		
+		</div><!-- end main container -->
+		
+		
+		<div id="footer">
+		
+		</div>
+		
+	</body>
+	
+	
+
+</html>
